1adb65dae8b5a08d5b0cbb064454c80c4c486151
POLK BRANDO VERNAZA QUIÑONEZ <83431658+o698polk@users.noreply.github.com>
2025-10-29 18:58:20 -0500

    Nuevos cambios

---
+++ PRACTICAS/APPMOVIL/AppHolamundo/AppHolamundo/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AppHolamundo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "AppHolamundo" };
+        }
+    }
+}
---
+++ PRACTICAS/APPMOVIL/AppHolamundo/AppHolamundo/Services/FormFactor.cs
@@ -0,0 +1,16 @@
+using AppHolamundo.Shared.Services;
+
+namespace AppHolamundo.Services;
+
+public class FormFactor : IFormFactor
+{
+    public string GetFormFactor()
+    {
+        return DeviceInfo.Idiom.ToString();
+    }
+
+    public string GetPlatform()
+    {
+        return DeviceInfo.Platform.ToString() + " - " + DeviceInfo.VersionString;
+    }
+}
